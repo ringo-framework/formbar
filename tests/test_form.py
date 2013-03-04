63ac0722a1a8a4a325676d234134141a02122800
Torsten Irländer <torsten@irlaender.de>
2013-03-04 21:58:06 +0100

    Added new tests on form validation and deserialisation

---
+++ tests/test_form.py
@@ -0,0 +1,57 @@
+import datetime
+import unittest
+from test_configparser import XML
+from formbar.config import parse, Config
+from formbar.form import Form
+
+
+class TestFormValidation(unittest.TestCase):
+
+    def setUp(self):
+        config = Config(parse(XML))
+        form_config = config.get_form('customform')
+        self.form = Form(form_config)
+
+    def test_form_init(self):
+        pass
+
+    def test_form_unknown_field(self):
+        values = {'unknown': 'test', 'age': '15', 'birthday': '1998-02-01'}
+        self.assertRaises(KeyError, self.form.validate, values)
+
+    def test_form_validate_fail(self):
+        values = {'default': 'test', 'age': '15', 'birthday': '1998-02-01'}
+        self.assertEqual(self.form.validate(values), False)
+
+    def test_form_validate_ok(self):
+        values = {'default': 'test', 'age': '16', 'birthday': '1998-02-01'}
+        self.assertEqual(self.form.validate(values), True)
+
+    def test_form_deserialize_int(self):
+        values = {'default': 'test', 'age': '16', 'birthday': '1998-02-01'}
+        self.form.validate(values)
+        self.assertEqual(self.form.data['age'], 16)
+
+    def test_form_deserialize_float(self):
+        values = {'default': 'test', 'age': '16',
+                  'birthday': '1998-02-01', 'weight': '87.5'}
+        self.assertEqual(self.form.validate(values), True)
+        self.assertEqual(self.form.data['weight'], 87.5)
+
+    def test_form_deserialize_date(self):
+        values = {'default': 'test', 'age': '16', 'birthday': '1998-02-01'}
+        self.form.validate(values)
+        self.assertEqual(self.form.data['birthday'], datetime.date(1998, 2, 1))
+
+    def test_form_deserialize_string(self):
+        values = {'default': 'test', 'age': '16', 'birthday': '1998-02-01'}
+        self.form.validate(values)
+        self.assertEqual(self.form.data['default'], 'test')
+
+    def test_form_save(self):
+        values = {'default': 'test', 'age': '16', 'birthday': '1998-02-01'}
+        self.assertEqual(self.form.validate(values), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
