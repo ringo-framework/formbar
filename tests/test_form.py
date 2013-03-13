18eeb2ae6c97ef79b2af322db7dc00161ba6671a
Torsten Irländer <torsten@keto.hq.intevation.de>
2013-03-13 08:08:51 +0100

    Added tests for forms with SQLAlchemy mapped items

--- tests/test_form.py
+++ tests/test_form.py
@@ -2,11 +2,42 @@ import os
 import datetime
 import unittest
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine('sqlite:///:memory:', echo=False)
+Session = scoped_session(sessionmaker())
+Session.configure(bind=engine)
+Base = declarative_base()
+
 from formbar import test_dir
 from formbar.config import load, Config
 from formbar.form import Form, StateError
 
+
 RESULT = """<div class="formbar-form"><form id="customform" class="testcss" method="GET" action="http://" autocomplete="off" >    \n        <div class="row-fluid"><tr>\n      \n        \n        <div class="span12">\n      \n\n        \n        \n        <label for="string">\n          String field\n        </label>\n          <input class="" id="string" name="string" type="text" value="" />\n\n        </div>\n\n        </div>\n        \n          \n        \n        <div class="row-fluid"><tr>\n      \n        \n        <div class="span12">\n      \n\n        \n        \n        <label for="default">\n          Default\n        </label>\n          <input class="" id="default" name="default" type="text" value="" />\n\n        </div>\n\n        </div>\n        <div class="row-fluid"><tr>\n      \n        \n        <div class="span6">\n      \n\n        \n        \n        <label for="float">\n          Float field\n        </label>\n          <input class="" id="float" name="float" type="text" value="" />\n        <div class="text-help">\n          <i class="icon-info-sign"></i>\n          This is is a very long helptext which should span over\n      multiple rows. Further the will check if there are further html\n      tags allowed.\n        </div>\n\n        </div>\n        \n        <div class="span6">\n      \n\n        \n        \n        <label for="date">\n            <sup>(1)</sup>\n          Date field\n        </label>\n          <div class="readonlyfield">\n            &nbsp;\n          </div>\n        <div class="text-help">\n          <i class="icon-info-sign"></i>\n          This is my helptext\n        </div>\n\n        </div>\n\n        </div>\n        \n          \n        \n        <div class="row-fluid"><tr>\n      \n        \n        <div class="span6">\n      \n\n        \n        \n        <label for="string">\n          String field\n        </label>\n          <input class="" id="string" name="string" type="text" value="" />\n\n        </div>\n        \n        <div class="span6">\n      \n\n        \n        \n        <label for="integer">\n          Integer field\n            <a href="#" data-toggle="tooltip" class="formbar-tooltip" data-original-title="Required field"><i class="icon-asterisk"></i></a>\n        </label>\n          <input class="" id="integer" name="integer" type="text" value="" />\n\n        </div>\n\n        </div>\n\n\n\n\n\n<script>\n  $(\'.formbar-tooltip\').tooltip();\n</script>\n<div class="row-fluid"><div class="span12 button-pane well-small"><button type="submit" class="btn btn-primary">Submit</button><button type="reset" class="btn btn-warning">Reset</button></div></div></form></div>"""
+
+
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(Integer)
+
+    def __init__(self, name=None, fullname=None, password=None):
+        self.name = name
+        self.fullname = fullname
+        self.password = password
+
+    def __repr__(self):
+       return "<User('%s','%s', '%s')>" % (self.name, self.fullname, self.password)
 
 
 class TestFormValidation(unittest.TestCase):
@@ -79,6 +110,63 @@ class TestFormRenderer(unittest.TestCase):
     def test_form_render(self):
         html = self.form.render()
         self.assertEqual(html, RESULT)
+
+class TestFormAlchemyForm(unittest.TestCase):
+
+    def _insert_item(self):
+        item = User('ed', 'Ed Jones', 'edspassword')
+        self.session.add(item)
+        self.session.commit()
+        return item
+
+    def setUp(self):
+        Base.metadata.create_all(engine)
+        tree = load(os.path.join(test_dir, 'form.xml'))
+        self.config = Config(tree)
+        self.session = Session()
+
+    def tearDown(self):
+        Session.remove()
+
+    def test_read(self):
+        form_config = self.config.get_form('userform1')
+        item = self._insert_item()
+        form = Form(form_config, item)
+        self.assertEqual(len(form.fs.render_fields), 2)
+
+    def test_create(self):
+        form_config = self.config.get_form('userform2')
+        item = User()
+        form = Form(form_config, item)
+        self.assertEqual(len(form.fs.render_fields), 3)
+
+    def test_create_save(self):
+        form_config = self.config.get_form('userform2')
+        item = User()
+        # Important! Provide the dbsession if you want to create a new
+        # item
+        form = Form(form_config, item, self.session)
+        values = {"name": "paul", "fullname": "Paul Wright", "password": "xxx"}
+        if form.validate(values):
+            saved_item = form.save()
+            self.assertEqual(saved_item, item)
+            result = self.session.query(User).all()
+            self.assertEqual(len(result), 1)
+            self.assertEqual(result[0].name, "paul")
+
+    def test_edit_save(self):
+        form_config = self.config.get_form('userform2')
+        item = self._insert_item()
+        item = self._insert_item()
+        result = self.session.query(User).all()
+        form = Form(form_config, item)
+        values = {"name": "paul", "fullname": "Paul Wright", "password": "xxx"}
+        if form.validate(values):
+            saved_item = form.save()
+            result = self.session.query(User).all()
+            self.assertEqual(len(result), 2)
+            self.assertEqual(result[0].name, "ed")
+            self.assertEqual(result[1].name, "paul")
 
 
 if __name__ == '__main__':
