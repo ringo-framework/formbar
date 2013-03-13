876e76c3e44d61449d71eb750c496ad427235349
Torsten Irländer <torsten@irlaender.de>
2013-03-14 00:21:22 +0100

    Added MANIFEST.in and changed setup.py file to include all needed files in the sdist.

--- setup.py
+++ setup.py
@@ -6,27 +6,28 @@ version = '0.0'
 setup(name='formbar',
       version=version,
       description="Python library to layout, render and validate HTML forms in web applications",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=open('README.rst').read(),
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+                   'Programming Language :: Python :: 2.7'],
       keywords='python form validate web html',
       author='Torsten Irl\xc3\xa4nder',
       author_email='torsten@irlaender.de',
-      url='',
+      url='https://github.com/toirl/formbar',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=['formbar'],
+      package_dir={'formbar': 'formbar'},
+      package_data={'formbar': ['templates/*.mako']},
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-        'pyparsing==1.5.6',
-        'formalchemy',
-        'formencode',
-        'mako'
-      ],
+      install_requires=['pyparsing==1.5.6',
+                        'formalchemy',
+                        'formencode',
+                        'mako'],
       # Used for the example server
-      extras_require = {
-        'examples':  ["pyramid"]
-      },
+      extras_require={'examples':  ["pyramid"]},
       entry_points="""
       # -*- Entry points: -*-
       """,
