c7cd02b589153b87dc14b7977717ef288e9d4b9e
Torsten Irländer <torsten.irlaender@intevation.de>
2013-07-12 16:55:07 +0200

    Added helper method get_js which returns the js code of all needed js code for formbar (including the libraries)

--- formbar/helpers.py
+++ formbar/helpers.py
@@ -9,3 +9,16 @@ def get_css():
     csspath = os.path.join(basepath, 'templates', 'formbar.css')
     with open(csspath, 'r') as f:
         return f.read()
+
+def get_js():
+    """Returns the content of the formbar JS file
+    :returns: String with js 
+    """
+    files = ['bootstrap-datepicker.js', 'formbar.js']
+    basepath = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+    js = []
+    for filename in files:
+        jspath = os.path.join(basepath, 'templates', filename)
+        with open(jspath, 'r') as f:
+            js.append(f.read())
+    return "".join(js)
