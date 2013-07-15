b06e3f83102c9f8e8d2a07526575db72deba8015
Torsten Irländer <torsten@irlaender.de>
2013-07-16 00:10:30 +0200

    Added a js helper function which is called when the user changes the currently selected page. The function will trigger a ajax request to the server which some parameters. The server will save the currently selected page in a session (or elsewhere depending on the implementation)

--- formbar/templates/formbar.js
+++ formbar/templates/formbar.js
@@ -2,3 +2,20 @@ $('.formbar-tooltip').tooltip();
 $('.formbar-datepicker').datepicker({
     format: 'yyyy-mm-dd'
 });
+/*
+* Set hidden form field "formbar-page" to the value of the currently
+* selected page. This value will be used to set the currently selected
+* page when the form ist rendered
+*/
+$('div.formbar-form form div.tabbable ul.nav li a').click(function() {
+  var page = $(this).attr('href').split('#p')[1];
+  var item = $(this).attr('formbar-item');
+  var itemid = $(this).attr('formbar-itemid');
+  $.get('/set_current_form_page', 
+        {
+            page: page,
+            item: item,
+            itemid: itemid
+        },
+        function(data, status) {});
+});
