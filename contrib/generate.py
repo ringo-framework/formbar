dde140a875364df7c435e3ee38173cdc798e8ad5
Torsten Irländer <torsten.irlaender@intevation.de>
2013-08-12 18:17:30 +0200

    Added helper script to generate various informations on the form configuration.

---
+++ contrib/generate.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from formbar.config import Config, Field, parse
+import sys, argparse
+
+def _get_config(config):
+    return Config(parse(config.read()))
+
+def _get_fields(config):
+    fields = []
+    for en in config.get_elements('entity'):
+        fields.append(Field(en))
+    return fields
+
+def print_model(config):
+    out = []
+    for field in _get_fields(config):
+        name = field.name
+        nullable = field.required == "true"
+        default = None
+        if field.type == "string":
+            dtype = "sa.Text"
+        elif field.type == "integer":
+            dtype = "sa.Integer"
+        elif field.type == "date":
+            dtype = "sa.Date"
+        elif field.type == "info":
+            continue # ignore this type
+        else:
+            dtype = "sa.Text"
+        out.append("sa.Column('%s', %s, nullable=%s, default=%r)" %
+                   (name, dtype, nullable, default))
+    print "\n".join(out)
+
+def main (config, action):
+    config_tree = _get_config(config)
+    if action == "model":
+        print_model(config_tree)
+    else:
+        print "nothing to do"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate various informations from a form configuration file')
+    parser.add_argument('action', choices=['model'], help='Output to generate')
+    parser.add_argument('config', metavar='config', type=file, help='A form configuration file')
+    args = parser.parse_args()
+    main(args.config, args.action)
+    sys.exit(0)
