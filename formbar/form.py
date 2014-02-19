6047f8770b68cd412c3c6cb93837fc56e5d0dc78
Torsten Irländer <torsten.irlaender@intevation.de>
2014-02-19 14:30:36 +0100

    Add hint to bug in formencode

--- formbar/form.py
+++ formbar/form.py
@@ -300,13 +300,10 @@ class Form(object):
         renderer = FormRenderer(self, self._translate)
         form = renderer.render(values=values, buttons=buttons,
                                previous_values=previous_values)
-        # FIXME: Workaround for
-        # https://github.com/formencode/formencode/pull/58 (ti)
-        # <2014-02-12 10:31>
         try:
             return htmlfill.render(form, values)
         except AssertionError, e:
-            log.error("Error while parsing form in htmlfill: %s" % e.message)
+            log.error("Error while parsing form in htmlfill: %s. Hint See 'https://github.com/formencode/formencode/issues/57'" % e.message)
 
     def _add_error(self, fieldname, error):
         field = self.get_field(fieldname)
