95a9ca19afcaf5950d9df08196443e5914b785a3
Torsten Irländer <torsten.irlaender@intevation.de>
2014-04-17 12:16:08 +0200

    Removed eval_url param from get_js function.

--- formbar/helpers.py
+++ formbar/helpers.py
@@ -18,21 +18,13 @@ def get_css():
     return "".join(css)
 
 
-def get_js(eval_url="undefined"):
+def get_js():
     """Returns the content of the formbar JS file
-
-    :eval_url: External URL for rule evaluation. If defined this URL is
-    called to evaluate client side rules with a AJAX request. The rule
-    to evaluate is provided in a GET request in the "rule" paramenter.
-    The return Value is a JSON response with success attribute set
-    depending on the result of the evaluation and the error message in
-    the data attribute in case the evaluation fails.
     :returns: String with js
-
     """
     files = ['bootstrap-datepicker.js', 'formbar.js']
     js = []
-    values = {'eval_url': eval_url}
+    values = {}
     for filename in files:
         template = template_lookup.get_template(filename)
         js.append(template.render(**values))
