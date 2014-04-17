1ab88b774b302f50c8f78f7af2ef7565bb4ebbc4
Torsten Irländer <torsten.irlaender@intevation.de>
2014-04-17 14:22:20 +0200

    Added new helper method which returns tuples of filenames and its content.

--- formbar/helpers.py
+++ formbar/helpers.py
@@ -4,28 +4,41 @@ from formbar import static_dir
 
 template_lookup = TemplateLookup(directories=[static_dir])
 
+def get_css_files():
+    files = ['css/datepicker3.css', 'css/formbar.css']
+    css = []
+    values = {}
+    for filename in files:
+        template = template_lookup.get_template(filename)
+        css.append((filename, template.render(**values)))
+    return css
+
+def get_js_files():
+    files = ['js/bootstrap-datepicker.js', 'js/formbar.js', 'js/ace/ace.js', 
+             'js/ace/ext-language_tools.js', 'js/ace/mode-xml.js', 
+             'js/ace/snippets/xml.js', 'js/ace/snippets/text.js']
+    js = []
+    for filename in files:
+        filepath = os.path.join(static_dir, filename)
+        with open(filepath, 'r') as f:
+            content = f.read()
+        js.append((filename, content))
+    return js
 
 def get_css():
     """Returns the content of the formbar CSS file
     :returns: String with css
     """
-    files = ['datepicker3.css', 'formbar.css']
-    css = []
-    values = {}
-    for filename in files:
-        template = template_lookup.get_template(filename)
-        css.append(template.render(**values))
-    return "".join(css)
-
+    out = []
+    for filename, content in get_css_files():
+        out.append(content)
+    return "".join(out)
 
 def get_js():
     """Returns the content of the formbar JS file
     :returns: String with js
     """
-    files = ['bootstrap-datepicker.js', 'formbar.js']
-    js = []
-    values = {}
-    for filename in files:
-        template = template_lookup.get_template(filename)
-        js.append(template.render(**values))
-    return "".join(js)
+    out = []
+    for filename, content in get_js_files():
+        out.append(content)
+    return "".join(out)
