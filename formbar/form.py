1eecf9a6c448d494909edce8408dbe8c5accc3d9
Torsten Irländer <torsten.irlaender@intevation.de>
2014-04-17 11:26:01 +0200

    Added eval_url parameter of form initialisation. Needed for client site rule evaluation.

--- formbar/form.py
+++ formbar/form.py
@@ -71,7 +71,7 @@ class Form(object):
 
     def __init__(self, config, item=None, dbsession=None, translate=None,
                  change_page_callback={}, renderers={}, request=None,
-                 csrf_token=None):
+                 csrf_token=None, eval_url=None):
         """Initialize the form with ``Form`` configuration instance and
         optional an SQLAlchemy mapped object.
 
@@ -91,13 +91,19 @@ class Form(object):
         when using in connection with ringo)
         :csrf_token: Token to which will be included as hidden field in
         the form to prevent CSRF attacks.
-
+        :eval_url: External URL for rule evaluation. If defined this URL is
+        called to evaluate client side rules with a AJAX request. The rule
+        to evaluate is provided in a GET request in the "rule" paramenter.
+        The return Value is a JSON response with success attribute set
+        depending on the result of the evaluation and the error message in
+        the data attribute in case the evaluation fails.
         """
         self._config = config
         self._item = item
         self._dbsession = dbsession
         self._request = request
         self._csrf_token = csrf_token
+        self._eval_url = eval_url
         if translate:
             self._translate = translate
         else:
