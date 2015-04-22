ffe72d92363a6b6c77fe450751ba1223d7c8bca7
Torsten Irländer <torsten.irlaender@intevation.de>
2015-04-22 18:14:32 +0200

    Escape value in get_value function to prevent HTML injection.

--- formbar/form.py
+++ formbar/form.py
@@ -1,5 +1,6 @@
 import logging
 import sqlalchemy as sa
+from markupsafe import escape
 from formbar.renderer import FormRenderer, get_renderer
 from formbar.rules import Rule
 from formbar.converters import (
@@ -574,7 +575,7 @@ class Field(object):
         self.previous_value = value
 
     def get_value(self, default=None, expand=False):
-        return self._get_value(self.value, default, expand)
+        return escape(self._get_value(self.value, default, expand))
 
     def get_previous_value(self, default=None, expand=False):
         return self._get_value(self.previous_value, default, expand)
