87b12d243372ff456fc6df0ccf246fe21c41e916
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-11-23 10:36:20 +0100

    Revert "Change default value for Selection fields of get_value to []"
    This reverts commit 5e7df379303a376481b8e461a2a2d1ee5e938eeb.

--- formbar/fields.py
+++ formbar/fields.py
@@ -558,17 +558,13 @@ class CollectionField(Field):
                     ex_values.append("%s" % opt[0])
         return ", ".join(ex_values)
 
-    def get_previous_value(self, default=None, expand=False):
-        if default is None:
-            default = []
+    def get_previous_value(self, default="", expand=False):
         value = super(CollectionField, self).get_previous_value(default)
         if expand:
             return self.expand_value(value)
         return value
 
-    def get_value(self, default=None, expand=False):
-        if default is None:
-            default = []
+    def get_value(self, default="", expand=False):
         value = super(CollectionField, self).get_value(default)
         if expand:
             return self.expand_value(value)
